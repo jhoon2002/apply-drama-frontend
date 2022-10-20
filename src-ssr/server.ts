2f54009e434ca7f7664181163302de59dc014c16
Yeo <jhoon2002@naver.com>
2022-10-21 00:43:54 +0900

    notebook

--- src-ssr/server.ts
+++ src-ssr/server.ts
@@ -9,15 +9,9 @@
  * Make sure to yarn add / npm install (in your project root)
  * anything you import here (except for express and compression).
  */
-import express from 'express';
-import compression from 'compression';
-import {
-  ssrClose,
-  ssrCreate,
-  ssrListen,
-  ssrRenderPreloadTag,
-  ssrServeStaticContent,
-} from 'quasar/wrappers';
+import express from 'express'
+import compression from 'compression'
+import { ssrClose, ssrCreate, ssrListen, ssrRenderPreloadTag, ssrServeStaticContent } from 'quasar/wrappers'
 
 /**
  * Create your webserver and return its instance.
@@ -27,20 +21,20 @@ import {
  * Should NOT be async!
  */
 export const create = ssrCreate((/* { ... } */) => {
-  const app = express();
+    const app = express()
 
-  // attackers can use this header to detect apps running Express
-  // and then launch specifically-targeted attacks
-  app.disable('x-powered-by');
+    // attackers can use this header to detect apps running Express
+    // and then launch specifically-targeted attacks
+    app.disable('x-powered-by')
 
-  // place here any middlewares that
-  // absolutely need to run before anything else
-  if (process.env.PROD) {
-    app.use(compression());
-  }
+    // place here any middlewares that
+    // absolutely need to run before anything else
+    if (process.env.PROD) {
+        app.use(compression())
+    }
 
-  return app;
-});
+    return app
+})
 
 /**
  * You need to make the server listen to the indicated port
@@ -54,13 +48,13 @@ export const create = ssrCreate((/* { ... } */) => {
  * handler for serverless use or whatever else fits your needs.
  */
 export const listen = ssrListen(async ({ app, port, isReady }) => {
-  await isReady();
-  return app.listen(port, () => {
-    if (process.env.PROD) {
-      console.log('Server listening at port ' + port);
-    }
-  });
-});
+    await isReady()
+    return app.listen(port, () => {
+        if (process.env.PROD) {
+            console.log('Server listening at port ' + port)
+        }
+    })
+})
 
 /**
  * Should close the server and free up any resources.
@@ -73,62 +67,62 @@ export const listen = ssrListen(async ({ app, port, isReady }) => {
  * Can be async.
  */
 export const close = ssrClose(({ listenResult }) => {
-  return listenResult.close();
-});
+    return listenResult.close()
+})
 
-const maxAge = process.env.DEV ? 0 : 1000 * 60 * 60 * 24 * 30;
+const maxAge = process.env.DEV ? 0 : 1000 * 60 * 60 * 24 * 30
 
 /**
  * Should return middleware that serves the indicated path
  * with static content.
  */
 export const serveStaticContent = ssrServeStaticContent((path, opts) => {
-  return express.static(path, {
-    maxAge,
-    ...opts,
-  });
-});
+    return express.static(path, {
+        maxAge,
+        ...opts,
+    })
+})
 
-const jsRE = /\.js$/;
-const cssRE = /\.css$/;
-const woffRE = /\.woff$/;
-const woff2RE = /\.woff2$/;
-const gifRE = /\.gif$/;
-const jpgRE = /\.jpe?g$/;
-const pngRE = /\.png$/;
+const jsRE = /\.js$/
+const cssRE = /\.css$/
+const woffRE = /\.woff$/
+const woff2RE = /\.woff2$/
+const gifRE = /\.gif$/
+const jpgRE = /\.jpe?g$/
+const pngRE = /\.png$/
 
 /**
  * Should return a String with HTML output
  * (if any) for preloading indicated file
  */
-export const renderPreloadTag = ssrRenderPreloadTag((file) => {
-  if (jsRE.test(file) === true) {
-    return `<link rel="modulepreload" href="${file}" crossorigin>`;
-  }
+export const renderPreloadTag = ssrRenderPreloadTag(file => {
+    if (jsRE.test(file) === true) {
+        return `<link rel="modulepreload" href="${file}" crossorigin>`
+    }
 
-  if (cssRE.test(file) === true) {
-    return `<link rel="stylesheet" href="${file}">`;
-  }
+    if (cssRE.test(file) === true) {
+        return `<link rel="stylesheet" href="${file}">`
+    }
 
-  if (woffRE.test(file) === true) {
-    return `<link rel="preload" href="${file}" as="font" type="font/woff" crossorigin>`;
-  }
+    if (woffRE.test(file) === true) {
+        return `<link rel="preload" href="${file}" as="font" type="font/woff" crossorigin>`
+    }
 
-  if (woff2RE.test(file) === true) {
-    return `<link rel="preload" href="${file}" as="font" type="font/woff2" crossorigin>`;
-  }
+    if (woff2RE.test(file) === true) {
+        return `<link rel="preload" href="${file}" as="font" type="font/woff2" crossorigin>`
+    }
 
-  if (gifRE.test(file) === true) {
-    return `<link rel="preload" href="${file}" as="image" type="image/gif">`;
-  }
+    if (gifRE.test(file) === true) {
+        return `<link rel="preload" href="${file}" as="image" type="image/gif">`
+    }
 
-  if (jpgRE.test(file) === true) {
-    return `<link rel="preload" href="${file}" as="image" type="image/jpeg">`;
-  }
+    if (jpgRE.test(file) === true) {
+        return `<link rel="preload" href="${file}" as="image" type="image/jpeg">`
+    }
 
-  if (pngRE.test(file) === true) {
-    return `<link rel="preload" href="${file}" as="image" type="image/png">`;
-  }
+    if (pngRE.test(file) === true) {
+        return `<link rel="preload" href="${file}" as="image" type="image/png">`
+    }
 
-  return '';
-});
+    return ''
+})
