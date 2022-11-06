c15d3426dbefd50df325b3c8866df0bc75aeaf89
Yeo <jhoon2002@naver.com>
2022-11-06 23:42:31 +0900

    notebook

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div class="fullscreen bg-teal-9 text-white text-center q-pa-md flex flex-center">
         <div>
-            <div style="font-size: 30vh">404</div>
+            <div class="text-h4 q-mb-lg" style="opacity: 0.4; font-weight: 100; letter-spacing: 13px">
+                404 Not Found
+            </div>
 
-            <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
+            <div class="text-body2" style="opacity: 0.4; letter-spacing: 8px">존재하지 않는 페이지 입니다</div>
 
-            <q-btn class="q-mt-xl" color="white" text-color="blue" unelevated to="/" label="Go Home" no-caps />
+            <q-btn class="q-mt-xl" color="teal-10" text-color="teal-4" unelevated to="/" label="Go Home" no-caps />
         </div>
     </div>
 </template>
