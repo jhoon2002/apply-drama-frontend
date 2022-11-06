eea92238be6decca1846e0fec5353326b55e4585
Yeo <jhoon2002@naver.com>
2022-11-06 23:48:59 +0900

    notebook

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -4,9 +4,7 @@
             <div class="text-h4 q-mb-lg" style="opacity: 0.4; font-weight: 100; letter-spacing: 13px">
                 404 Not Found
             </div>
-
             <div class="text-body2" style="opacity: 0.4; letter-spacing: 8px">존재하지 않는 페이지 입니다</div>
-
             <q-btn class="q-mt-xl" color="teal-10" text-color="teal-4" unelevated to="/" label="Go Home" no-caps />
         </div>
     </div>
